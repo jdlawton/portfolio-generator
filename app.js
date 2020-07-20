de4fd3e6494aeac1f33b6c7ea0e1d2efbb5a02a4
JoeL <jlawton1203@gmail.com>
2020-07-20 11:16:30 -0500

    added html page generation in generatePage module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Module 1 Code
+/*
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const printProfileData = profileDataArr => {
@@ -10,3 +12,16 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+*/
+
+//Module 2 Code
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
